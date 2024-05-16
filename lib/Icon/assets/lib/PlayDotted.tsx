208dd727acd818fc6739803ec012f53e22f84879
Jamil Alisgandarov <19148254+jamilalisgandarov@users.noreply.github.com>
2024-05-16 12:12:46 +0400

    Fix linter issues

--- lib/Icon/assets/lib/PlayDotted.tsx
+++ lib/Icon/assets/lib/PlayDotted.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const PlayDotted = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- lib/Icon/assets/lib/X.tsx
+++ lib/Icon/assets/lib/X.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const X = (props: SVGProps<SVGSVGElement>) => (
   <svg
