d9552327138710b17a5079b989cdb80d4b6c643b
Jamil Alisgandarov <19148254+jamilalisgandarov@users.noreply.github.com>
2024-05-17 00:07:43 +0400

    Update Badge.stories.tsx

--- lib/Info/Badge/Badge.stories.tsx
+++ lib/Info/Badge/Badge.stories.tsx
@@ -10,7 +10,7 @@ const meta = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
     backgrounds: {
-      default: "white",
+      default: "black",
     },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
