9e3718f5c2906755d3f84e80b0dc2b980a65693d
Jamil Alisgandarov <jamil.alsgndrv@gmail.com>
2024-06-08 00:04:58 +0400

    Added new play icon

---
+++ lib/Icon/assets/lib/PlayFilled.tsx
@@ -0,0 +1,18 @@
+import type { SVGProps } from "react";
+const PlayFilled = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    color="#000"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M4.624 3.351a.75.75 0 0 1 .748-.002l14 8a.75.75 0 0 1 0 1.302l-14 8A.75.75 0 0 1 4.25 20V4a.75.75 0 0 1 .374-.649"
+    />
+  </svg>
+);
+export default PlayFilled;
