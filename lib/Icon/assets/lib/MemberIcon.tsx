59bb9694d09ea8f9e980d9ad0eb22a10e938d165
nikitagudkovs <nikita.gudkovs@gmail.com>
2024-09-04 14:01:23 +0200

    member icon 2

--- lib/Icon/assets/lib/MemberIcon.tsx
+++ lib/Icon/assets/lib/MemberIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import newIconPng from '../pngs/member_icon.png/'
+import newIconPng from '../pngs/member_icon.png'; // Ensure the correct path and module declaration
 
 const NewIcon: React.FC = () => (
     <img src={newIconPng} alt="New Icon" />
